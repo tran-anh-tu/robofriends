9c34b18142c6c19fafb2da5d8329007b90a60246
Anh Tu <trananhtu0103@gmail.com>
2020-08-30 17:11:46 +0700

    Updates

---
+++ precache-manifest.bec92604e496474dfca3de57263527b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c287bbe3a3bfd6463f03d310a034b717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "885cbe5ab336a6b5685f",
+    "url": "/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "31bdac2a26c1702a32d7",
+    "url": "/static/css/main.93bae99b.chunk.css"
+  },
+  {
+    "revision": "885cbe5ab336a6b5685f",
+    "url": "/static/js/2.8e6ea57e.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/static/js/2.8e6ea57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bdac2a26c1702a32d7",
+    "url": "/static/js/main.1bdbd7fa.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
