1e25e50e8fd6afd03824e683b49da980a9a98d3a
Anh Tu <trananhtu0103@gmail.com>
2020-08-30 20:22:46 +0700

    first commit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,11 +4,13 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
-        type='search'
-        placeholder='search robots'
-        onChange={searchChange}
-      />
+      aria-label='Search'
+      className='pa3 ba b--green bg-lightest-blue'
+      type='search'
+      placeholder='search robots'
+      onChange={searchChange}
+    />
+   
     </div>
   );
 }
